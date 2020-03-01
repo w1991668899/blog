7bbb308bbc5c9175a172795811462df29ed2c056
wentao <1991668899@qq.com>
2020-03-01 21:46:23 +0800

    blogg

--- go/main.go
+++ go/main.go
@@ -1,28 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
 
-//Examples:
-//%f     default width, default precision
-//%9f    width 9, default precision
-//%.2f   default width, precision 2
-//%9.2f  width 9, precision 2
-//%9.f   width 9, precision 0
-
-//如下示例
+//紧跟在verb之前的[n]符号表示应格式化第n个参数（索引从1开始）。同样的在'*'之前的[n]符号表示采用第n个参数的值作为宽度或精度。在处理完方括号表达式[n]后，除非另有指示，会接着处理参数n+1，n+2……（就是说移动了当前处理位置）
 func main() {
-	fmt.Printf("%b\n", 10.45)    //5882827013252710p-49
-	fmt.Printf("%e\n", 10.45)    //1.045000E+01
-	fmt.Printf("%E\n", 10.45)    //1.045000E+01
-	fmt.Printf("%f\n", 10.451)   //10.451000
-	fmt.Printf("%9f\n", 10.45)   //10.450000
-	fmt.Printf("%9.2f\n", 10.45) //     10.45
-	fmt.Printf("%9.f\n", 10.45)  //        10
-	fmt.Printf("%.1f\n", 10.45)  //10.4
-	fmt.Printf("%.4f\n", 10.45)  //10.4500
-	fmt.Printf("%F\n", 10.45)    //10.450000
-	fmt.Printf("%g\n", 10.45)    //10.45
-	fmt.Printf("%G\n", 10.45)    //10.45
-	fmt.Printf("%x\n", 10.45)    //0x1.4e66666666666p+03
-	fmt.Printf("%X\n", 10.45)    //0X1.4E66666666666P+03
+	fmt.Printf("%[2]d %[1]d\n", 11, 22)
+	fmt.Printf("%[3]*.[2]*[1]f\n", 12.0, 2, 6)
+	fmt.Printf("%d %d %#[1]x %#x\n", 16, 17)
 }
+
+func TestErrorf(t *testing.T) {
+	wrapped := errors.New("inner error")
+	for _, test := range []struct {
+		err        error
+		wantText   string
+		wantUnwrap error
+	}{{
+		err:        fmt.Errorf("%w", wrapped),
+		wantText:   "inner error",
+		wantUnwrap: wrapped,
+	}, {
+		err:        fmt.Errorf("added context: %w", wrapped),
+		wantText:   "added context: inner error",
+		wantUnwrap: wrapped,
+	}, {
+		err:        fmt.Errorf("%w with added context", wrapped),
+		wantText:   "inner error with added context",
+		wantUnwrap: wrapped,
+	}, {
+		err:        fmt.Errorf("%s %w %v", "prefix", wrapped, "suffix"),
+		wantText:   "prefix inner error suffix",
+		wantUnwrap: wrapped,
+	}, {
+		err:        fmt.Errorf("%[2]s: %[1]w", wrapped, "positional verb"),
+		wantText:   "positional verb: inner error",
+		wantUnwrap: wrapped,
+	}, {
+		err:      fmt.Errorf("%v", wrapped),
+		wantText: "inner error",
+	}, {
+		err:      fmt.Errorf("added context: %v", wrapped),
+		wantText: "added context: inner error",
+	}, {
+		err:      fmt.Errorf("%v with added context", wrapped),
+		wantText: "inner error with added context",
+	}, {
+		err:      fmt.Errorf("%w is not an error", "not-an-error"),
+		wantText: "%!w(string=not-an-error) is not an error",
+	}, {
+		err:      fmt.Errorf("wrapped two errors: %w %w", errString("1"), errString("2")),
+		wantText: "wrapped two errors: 1 %!w(fmt_test.errString=2)",
+	}, {
+		err:      fmt.Errorf("wrapped three errors: %w %w %w", errString("1"), errString("2"), errString("3")),
+		wantText: "wrapped three errors: 1 %!w(fmt_test.errString=2) %!w(fmt_test.errString=3)",
+	}, {
+		err:        fmt.Errorf("%w", nil),
+		wantText:   "%!w(<nil>)",
+		wantUnwrap: nil, // still nil
+	}} {
+		if got, want := errors.Unwrap(test.err), test.wantUnwrap; got != want {
+			t.Errorf("Formatted error: %v\nerrors.Unwrap() = %v, want %v", test.err, got, want)
+		}
+		if got, want := test.err.Error(), test.wantText; got != want {
+			t.Errorf("err.Error() = %q, want %q", got, want)
+		}
+	}
+}
+
+type errString string
+
+func (e errString) Error() string { return string(e) }
